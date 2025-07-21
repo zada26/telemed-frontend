8e72c63e2468a7d1c3d3d237e0d67c486b818182
zada26 <zada26@aol.com>
2025-07-20 20:57:16 -0400

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Welcome to My Telemedicine Clinic</h1>
+      <p>Secure, convenient care from anywhere.</p>
+      <p><a href="/login">Doctor Login</a> | <a href="/register">Patient Register</a></p>
+    </div>
+  );
+}
